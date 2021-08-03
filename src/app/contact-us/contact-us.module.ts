663f1dfe3385bddf7abc9e06939e9776e9e2dd3c
Ashik tm <ashik.tm@electronikmedia.com>
2021-08-03 22:49:30 +0530

    forms changes

--- src/app/contact-us/contact-us.module.ts
+++ src/app/contact-us/contact-us.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 import { ContactUsRoutingModule } from './contact-us-routing.module';
 import { ContactUsHomeComponent } from './contact-us-home/contact-us-home.component';
 
-
 @NgModule({
   declarations: [ContactUsHomeComponent],
-  imports: [CommonModule, ContactUsRoutingModule],
+  imports: [CommonModule, ContactUsRoutingModule, MatIconModule],
 })
 export class ContactUsModule {}
