adeeef512c709780ba12f2bc5cd771812ef91501
Ashik tm <ashik.tm@electronikmedia.com>
2021-08-07 15:26:21 +0530

    dmdd

--- src/app/about-us/about-us.module.ts
+++ src/app/about-us/about-us.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AboutUsRoutingModule } from './about-us-routing.module';
 import { AboutHomeComponent } from './about-home/about-home.component';
 
-
 @NgModule({
-  declarations: [
-    AboutHomeComponent
-  ],
-  imports: [
-    CommonModule,
-    AboutUsRoutingModule
-  ]
+  declarations: [AboutHomeComponent],
+  imports: [CommonModule, AboutUsRoutingModule, MatIconModule],
 })
-export class AboutUsModule { }
+export class AboutUsModule {}
